762988710d9ecec40c817c1c771a23e08289f0f3
REY-MD <humphreymbise01@gmail.com>
2026-04-04 17:04:48 +0300

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,101 @@
+const config = require('../config')
+const { cmd, commands } = require('../command');
+const path = require('path'); 
+const os = require("os")
+const fs = require('fs');
+const {runtime} = require('../lib/functions')
+const axios = require('axios')
+
+cmd({
+    pattern: "menu",
+    alias: ["allmenu","fullmenu"],
+    use: '.menu',
+    desc: "Show all bot commands",
+    category: "menu",
+    react: "📜",
+    filename: __filename
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let dec = `╭━━〔 🚀 *${config.BOT_NAME}* 〕━━┈⊷
+┃◈╭─────────────────·๏
+┃◈┃• 👑 Owner : *${config.OWNER_NAME}*
+┃◈┃• ⚙️ Prefix : *[${config.PREFIX}]*
+┃◈┃• 🌐 Platform : *Heroku*
+┃◈┃• 📦 Version : *4.0.0*
+┃◈┃• ⏱️ Runtime : *${runtime(process.uptime())}*
+┃◈╰─────────────────┈⊷
+╰━━━━━━━━━━━━━━━━━━━┈⊷
+
+╭━━〔 ⚡ *MAIN MENU* 〕━━┈⊷
+┃◈╭─────────────────·๏
+┃◈┃• 🏓 menu
+┃◈┃• 🏓 ping
+┃◈┃• 🚀 speed
+┃◈┃• 📡 live
+┃◈┃• 💚 alive
+┃◈┃• ⏱️ runtime
+┃◈┃• ⏳ uptime
+┃◈┃• 📦 repo
+┃◈┃• 👑 owner
+┃◈┃• 📜 play
+┃◈┃• 📜 zeze
+┃◈┃• 🔄 restart
+┃◈╰─────────────────┈⊷
+╰━━━━━━━━━━━━━━━━━━━┈⊷
+
+╭━━〔 🤖 *TABLE* 〕━━┈⊷
+┃◈╭─────────────────·๏
+┃◈┃• 🧠 ai
+┃◈┃• 🤖 gpt
+┃◈┃• 🤖 gptmini
+┃◈┃• 🔵 meta
+┃◈┃• 📦 blackbox
+┃◈┃• 💢ping
+┃◈┃• ❣️
+┃◈┃• 🗯
+┃◈┃• 💭
+┃◈┃• 👑
+┃◈┃• 💫
+┃◈┃• 💥
+┃◈┃• 🌍
+┃◈┃• 🎨 
+┃◈┃• 🖼️
+┃◈┃• 🤖 
+┃◈╰─────────────────┈⊷
+╰━━━━━━━━━━━━━━━━━━━┈⊷
+
+> ${config.DESCRIPTION}`;
+
+        await conn.sendMessage(
+            from,
+            {
+                image: { url: config.MENU_IMAGE_URL || '.jpg' },
+                caption: dec,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '@newsletter',
+                        newsletterName: config.BOT_NAME,
+                        serverMessageId: 143
+                    }
+                }
+            },
+            { quoted: mek }
+        );
+// share local audio 
+
+const audioPath = path.join(__dirname, '../assets/menu.m4a');
+await conn.sendMessage(from, {
+    audio: fs.readFileSync(audioPath),
+    mimetype: 'audio/mp4',
+    ptt: false,
+}, { quoted: mek });
+        
+    } catch (e) {
+        console.log(e);
+        reply(`❌ Error: ${e}`);
+    }
+});
