aabc9ac4ca88b95226aab8ab3c2681a8c7a65450
REY-MD <humphreymbise01@gmail.com>
2026-04-04 16:44:08 +0300

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,60 @@
+const { cmd } = require('../command');
+const config = require('../config');
+
+cmd({
+    pattern: "owner",
+    react: "👑", 
+    desc: "Get bot owner contact",
+    category: "main",
+    filename: __filename
+}, 
+async (conn, mek, m, { from }) => {
+    try {
+        const ownerNumber = config.OWNER_NUMBER;
+        const ownerName = config.OWNER_NAME || "ZEZE TECH_";
+
+        // vCard
+        const vcard = 
+`BEGIN:VCARD
+VERSION:3.0
+FN:${ownerName}
+ORG: zezetech;
+TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}
+END:VCARD`;
+
+        // Styled caption message
+        const caption = `
+*╭ׂ┄─̇─̣┄─̇─̣┄─̇─̣┄─̇─̣┄─̇─̣─̇─̣─᛭*
+*│ ╌─̇─̣⊰ ZEZE-MD_V5 ⊱┈─̇─̣╌*
+*│─̇─̣┄┄┄┄┄┄┄┄┄┄┄┄┄─̇─̣*
+*│👑 𝐎𝐖𝐍𝐄𝐑 𝐂𝐎𝐍𝐓𝐀𝐂𝐓*
+*│*
+*│📛 𝐍𝐚𝐦𝐞:* ${ownerName}
+*│📞 𝐍𝐮𝐦𝐛𝐞𝐫:* ${ownerNumber}
+*│*
+*│💬 Tap contact to chat*
+*╰┄─̣┄─̇─̣┄─̇─̣┄─̇─̣┄─̇─̣─̇─̣─᛭*
+
+> ZEZE TECH
+`;
+
+        // Send styled text
+        await conn.sendMessage(from, {
+            text: caption
+        }, { quoted: mek });
+
+        // Send contact card (ONLY contact, no extra data)
+        await conn.sendMessage(from, {
+            contacts: {
+                displayName: ownerName,
+                contacts: [{ vcard }]
+            }
+        }, { quoted: mek });
+
+    } catch (error) {
+        console.error("OWNER CMD ERROR:", error);
+        await conn.sendMessage(from, {
+            text: "❌ Owner command error, please try again later."
+        }, { quoted: mek });
+    }
+});
