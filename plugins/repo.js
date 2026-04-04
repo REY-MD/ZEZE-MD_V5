5371ac286b1d24f131c105fe83aae3405808cba8
REY-MD <humphreymbise01@gmail.com>
2026-04-04 22:46:36 +0300

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -1,1 +1,73 @@
+const config = require('../config')
+const { cmd } = require('../command')
+const os = require("os")
+const { runtime, sleep } = require('../lib/functions')
+const axios = require('axios')
 
+cmd({
+    pattern: "repo",
+    alias: ["sc", "script", "repository"],
+    desc: "Show the bot's GitHub repository",
+    react: "📂",
+    category: "info",
+    filename: __filename,
+},
+async (conn, mek, m, { from, reply }) => {
+    const githubRepoURL = '.github';
+
+    try {
+        const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
+
+        const response = await axios.get(``);
+        const repoData = response.data;
+
+        const formattedInfo = `
+╭─〔 *PK-XMD REPOSITORY* 〕
+│
+├─ *📌 Repo Name:* ${repoData.name}
+├─ *👤 Owner:* ${repoData.owner.login}
+├─ *⭐ Stars:* ${repoData.stargazers_count}
+├─ *⑂ Forks:* ${repoData.forks_count}
+├─ *📄 Description:* ${repoData.description || 'Powerful WhatsApp Multi-Device Bot by ZEZE TECH'}
+│
+├─ *🔗 GitHub Link:*
+│   ${repoData.html_url}
+│
+├─ *🌍 Channel:*
+│   
+│
+╰─ *🚀 Powered by Pkdriller*
+`.trim();
+
+        await conn.sendMessage(from, {
+            image: { url: `` }, // you can change image
+            caption: formattedInfo,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '@newsletter',
+                    newsletterName: 'ZEZE UPDATES',
+                    serverMessageId: 110
+                }
+            }
+        }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "ZEZE TECH VERIFIED",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:ZEZE;BOT;;;\nFN:ZEZE\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+
+    } catch (error) {
+        console.error("❌ Error fetching repo:", error);
+        reply("❌ Failed to fetch repository info. Please try again later.");
+    }
+});
