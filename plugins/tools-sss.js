dfd64f00a60df36313369f0172f14b6024f167f9
REY-MD <humphreymbise01@gmail.com>
2026-04-06 02:19:12 +0300

    Update and rename a.js to tools-sss.js

---
+++ plugins/tools-sss.js
@@ -0,0 +1,52 @@
+
+
+const axios = require("axios");
+const config = require('../config');
+const { cmd } = require('../command');
+
+cmd({
+  pattern: "sss",
+  alias: ["ssweb"],
+  react: "💫",
+  desc: "Download screenshot of a given link.",
+  category: "other",
+  use: ".ss <link>",
+  filename: __filename,
+}, 
+async (conn, mek, m, {
+  from, l, quoted, body, isCmd, command, args, q, isGroup, sender, 
+  senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, 
+  groupMetadata, groupName, participants, isItzcp, groupAdmins, 
+  isBotAdmins, isAdmins, reply 
+}) => {
+  if (!q) {
+    return reply("Please provide a URL to capture a screenshot.");
+  }
+
+  try {
+    // created by jawad tech 
+    const response = await axios.get(`https://api.davidcyriltech.my.id/ssweb?url=${q}`);
+    const screenshotUrl = response.data.screenshotUrl;
+
+    // give credit and use
+    const imageMessage = {
+      image: { url: screenshotUrl },
+      caption: "*𝐙𝐄𝐙𝐄-𝐌𝐃_𝐕𝟓 DOWNLOADER*\n\n> *𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇*",
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363295141350550@newsletter',
+          newsletterName: "𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇",
+          serverMessageId: 143,
+        },
+      },
+    };
+
+    await conn.sendMessage(from, imageMessage, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    reply("Failed to capture the screenshot. Please try again.");
+  }
+});
