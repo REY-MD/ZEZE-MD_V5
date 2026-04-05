77f95492ddb3375d312a6c24ac3bb7128e52134e
REY-MD <humphreymbise01@gmail.com>
2026-04-05 15:21:26 +0300

    Update quran.js

--- plugins/quran.js
+++ plugins/quran.js
@@ -57,7 +57,7 @@ ${translatedTafsirEnglish.text}`;
           forwardingScore: 999,
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
-            newsletterJid: '@newsletter',
+            newsletterJid: '120363295141350550@newsletter',
             newsletterName: '𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇',
             serverMessageId: 143
           }
