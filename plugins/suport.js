cc39d63319acd80a7668a674c2fe80af9a88fc83
REY-MD <humphreymbise01@gmail.com>
2026-04-05 23:09:12 +0300

    Create suport.js

---
+++ plugins/suport.js
@@ -0,0 +1,80 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const axios = require('axios');
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+cmd({
+    pattern: "support",
+    alias : "version",
+    desc: " allmenu",
+    category: "allmenu",
+    react: "🫅",
+    filename: __filename
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+let dec = `    
+⟣──────────────────⟢
+▧ *ᴄʀᴇᴀᴛᴏʀ* : *𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇 (🇹🇿)*
+▧ *ᴍᴏᴅᴇ* : *${config.MODE}*
+▧ *ᴘʀᴇғɪx* : *${config.PREFIX}*
+▧ *ʀᴀᴍ* : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}GB / ${Math.round(require('os').totalmem / 1024 / 1024)}GB
+▧ *ᴠᴇʀsɪᴏɴ* : *V.5* ⚡
+▧ *ᴜᴘᴛɪᴍᴇ* : ${runtime(process.uptime())}
+
+⟣──────────────────⟢
+
+> 𝐙𝐄𝐙𝐄-𝐌𝐃_𝐕𝟓
+
+⟣──────────────────⟢
+${readMore}
+\`CHANNEL🛠️\`
+https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r
+
+\`GROUP\` 👥
+
+
+
+\`𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇\` *Dev🧑‍💻*
+
+wa.me/+255617657675?text=Support!
+
+⟣──────────────────⟢
+
+`;
+
+await conn.sendMessage(
+            from,
+            {
+                image: { url: `https://files.catbox.moe/sez5vx.jpg` },
+                caption: dec,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363295141350550@newsletter',
+                        newsletterName: '『 𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇 』',
+                        serverMessageId: 143
+                    }
+                }
+            },
+            { quoted: mek }
+        );
+
+        // Send audio
+        await conn.sendMessage(from, {
+            audio: { url: 'https://files.catbox.moe/a1sh4u.mp3' },
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
+        
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
