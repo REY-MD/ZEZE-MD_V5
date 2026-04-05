1bb6e60c6bbd199f40b67bb6cc3ee39b1f60a0d9
REY-MD <humphreymbise01@gmail.com>
2026-04-06 02:59:40 +0300

    Update version.js

--- plugins/version.js
+++ plugins/version.js
@@ -28,7 +28,7 @@ cmd({
 
     // Fetch latest version data from GitHub
     const rawVersionUrl = 'https://raw.githubusercontent.com/REY-MD/ZEZE-MD_V5/main/data/version.json';
-    let latestVersion = 'Unknown';
+    let latestVersion = '4.01';
     let latestChangelog = 'No changelog available.';
     try {
       const { data } = await axios.get(rawVersionUrl);
