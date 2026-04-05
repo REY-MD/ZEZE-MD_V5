53ed3432a48fc498b381c6a8565c09bf23cf5022
REY-MD <humphreymbise01@gmail.com>
2026-04-05 13:34:59 +0300

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -44,7 +44,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${mem.id.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ ZEZE ┃ TECH ✪──";
+        teks += "└──✪ 𝐙𝐄𝐙𝐄 ┃ 𝐓𝐄𝐂𝐇 ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: participants.map(a => a.id) }, { quoted: mek });
 
