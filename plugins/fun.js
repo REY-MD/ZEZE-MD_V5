2303a236f886f8c07efc983928ac59c97fa8749b
REY-MD <humphreymbise01@gmail.com>
2026-04-11 13:45:09 +0300

    Update fun.js

--- plugins/fun.js
+++ plugins/fun.js
@@ -18,7 +18,7 @@ cmd({
       return reply("❌ Failed to fetch a joke. Please try again.");
     }
 
-    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *ᗩᗪᗴᗴᒪ-᙭ᗰᗪ*`;
+    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇*`;
 
     return reply(jokeMessage);
   } catch (error) {
@@ -166,7 +166,7 @@ cmd({
       return reply("❌ Failed to fetch a fun fact. Please try again.");
     }
 
-    const factMessage = `🧠 *Random Fun Fact* 🧠\n\n${fact}\n\nIsn't that interesting? 😄\n\n> *ᗩᗪᗴᗴᒪ-᙭ᗰᗪ*`;
+    const factMessage = `🧠 *Random Fun Fact* 🧠\n\n${fact}\n\nIsn't that interesting? 😄\n\n> *𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇*`;
 
     return reply(factMessage);
   } catch (error) {
