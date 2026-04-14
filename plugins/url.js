5b08f30c8ac713d32a8121a34b303e6a895da9d2
REY-MD <humphreymbise01@gmail.com>
2026-04-14 14:42:14 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -6,7 +6,7 @@ const path = require("path");
 const { cmd, commands } = require("../command");
 
 cmd({
-  'pattern': "url",
+  'pattern': "tourl",
   'alias': ["imgtourl", "imgurl", "url", "geturl", "upload"],
   'react': '🖇',
   'desc': "Convert media to Catbox URL",
@@ -18,7 +18,7 @@ cmd({
     // Check if quoted message exists and has media
     const quotedMsg = message.quoted ? message.quoted : message;
     const mimeType = (quotedMsg.msg || quotedMsg).mimetype || '';
-    
+
     if (!mimeType) {
       throw "Please reply to an image, video, or audio file";
     }
@@ -34,7 +34,7 @@ cmd({
     else if (mimeType.includes('image/png')) extension = '.png';
     else if (mimeType.includes('video')) extension = '.mp4';
     else if (mimeType.includes('audio')) extension = '.mp3';
-    
+
     const fileName = `file${extension}`;
 
     // Prepare form data for Catbox
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by 𝐙𝐄𝐙𝐄-𝐓𝐄𝐂𝐇`
+      `> © Uploaded by Zeze Tech Ltd`
     );
 
   } catch (error) {
